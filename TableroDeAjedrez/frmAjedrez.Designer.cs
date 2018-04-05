fd75571b19f6e1e9330c1a324923d1324756b996
tic-rt16311016@uthermosillo.edu.mx <tic-rt16311016@uthermosillo.edu.mx>
2018-04-04 21:52:08 -0700

    Otros cambios

---
+++ TableroDeAjedrez/frmAjedrez.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace TableroDeAjedrez
+{
+    partial class frmAjedrez
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnTABLERO = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbFICHASNEGRAS = new System.Windows.Forms.RadioButton();
+            this.gbSELECCIONFICHAS = new System.Windows.Forms.GroupBox();
+            this.rbFICHASBLANCAS = new System.Windows.Forms.RadioButton();
+            this.btnREINICIAR = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.gbSELECCIONFICHAS.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnTABLERO
+            // 
+            this.pnTABLERO.Location = new System.Drawing.Point(54, 151);
+            this.pnTABLERO.Name = "pnTABLERO";
+            this.pnTABLERO.Size = new System.Drawing.Size(482, 403);
+            this.pnTABLERO.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tablero de Ajedrez";
+            // 
+            // rbFICHASNEGRAS
+            // 
+            this.rbFICHASNEGRAS.AutoSize = true;
+            this.rbFICHASNEGRAS.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rbFICHASNEGRAS.Location = new System.Drawing.Point(6, 25);
+            this.rbFICHASNEGRAS.Name = "rbFICHASNEGRAS";
+            this.rbFICHASNEGRAS.Size = new System.Drawing.Size(128, 24);
+            this.rbFICHASNEGRAS.TabIndex = 2;
+            this.rbFICHASNEGRAS.TabStop = true;
+            this.rbFICHASNEGRAS.Text = "Fichas Negras";
+            this.rbFICHASNEGRAS.UseVisualStyleBackColor = true;
+            // 
+            // gbSELECCIONFICHAS
+            // 
+            this.gbSELECCIONFICHAS.Controls.Add(this.rbFICHASBLANCAS);
+            this.gbSELECCIONFICHAS.Controls.Add(this.rbFICHASNEGRAS);
+            this.gbSELECCIONFICHAS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbSELECCIONFICHAS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbSELECCIONFICHAS.Location = new System.Drawing.Point(54, 62);
+            this.gbSELECCIONFICHAS.Name = "gbSELECCIONFICHAS";
+            this.gbSELECCIONFICHAS.Size = new System.Drawing.Size(300, 60);
+            this.gbSELECCIONFICHAS.TabIndex = 3;
+            this.gbSELECCIONFICHAS.TabStop = false;
+            this.gbSELECCIONFICHAS.Text = "Seleccione el color de las fichas:";
+            // 
+            // rbFICHASBLANCAS
+            // 
+            this.rbFICHASBLANCAS.AutoSize = true;
+            this.rbFICHASBLANCAS.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rbFICHASBLANCAS.Location = new System.Drawing.Point(153, 25);
+            this.rbFICHASBLANCAS.Name = "rbFICHASBLANCAS";
+            this.rbFICHASBLANCAS.Size = new System.Drawing.Size(134, 24);
+            this.rbFICHASBLANCAS.TabIndex = 3;
+            this.rbFICHASBLANCAS.TabStop = true;
+            this.rbFICHASBLANCAS.Text = "Fichas Blancas";
+            this.rbFICHASBLANCAS.UseVisualStyleBackColor = true;
+            // 
+            // btnREINICIAR
+            // 
+            this.btnREINICIAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnREINICIAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnREINICIAR.Location = new System.Drawing.Point(437, 74);
+            this.btnREINICIAR.Name = "btnREINICIAR";
+            this.btnREINICIAR.Size = new System.Drawing.Size(99, 48);
+            this.btnREINICIAR.TabIndex = 4;
+            this.btnREINICIAR.Text = "Reiniciar";
+            this.btnREINICIAR.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(372, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(309, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(251, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(193, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(497, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(434, 132);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "7";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(30, 172);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 16);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "A";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(30, 218);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 16);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "B";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(29, 516);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 16);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "H";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(28, 470);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(19, 16);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "G";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(31, 419);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(17, 16);
+            this.label18.TabIndex = 21;
+            this.label18.Text = "F";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(30, 373);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(18, 16);
+            this.label19.TabIndex = 22;
+            this.label19.Text = "E";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(28, 324);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(19, 16);
+            this.label20.TabIndex = 23;
+            this.label20.Text = "D";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(30, 272);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(18, 16);
+            this.label21.TabIndex = 24;
+            this.label21.Text = "C";
+            // 
+            // frmAjedrez
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 587);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnREINICIAR);
+            this.Controls.Add(this.gbSELECCIONFICHAS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnTABLERO);
+            this.Name = "frmAjedrez";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ajedrez";
+            this.Load += new System.EventHandler(this.frmAjedrez_Load);
+            this.gbSELECCIONFICHAS.ResumeLayout(false);
+            this.gbSELECCIONFICHAS.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnTABLERO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbFICHASNEGRAS;
+        private System.Windows.Forms.GroupBox gbSELECCIONFICHAS;
+        private System.Windows.Forms.RadioButton rbFICHASBLANCAS;
+        private System.Windows.Forms.Button btnREINICIAR;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+    }
+}
+
---
+++ TableroDeAjedrez/frmAjedrez.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableroDeAjedrez
+{
+    public partial class frmAjedrez : Form
+    {
+        public frmAjedrez()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Dibuja el tablero en el panel de 8x8 en blanco y negro.
+        /// </summary>
+        private void FormarTablero()
+        {
+            Size _size = new Size(pnTABLERO.Width / 8, pnTABLERO.Height / 8);
+            for (int f = 0; f < 8; f++)
+                for (int c = 0; c < 8; c++)
+                {
+                    var panel = new Panel();
+                   panel.Size = _size;
+                   panel.Location = new Point(c * _size.Width, f * _size.Height);
+                   panel.BackColor = (c + f) % 2 == 0 ? Color.Black : Color.White;
+                    pnTABLERO.Controls.Add(panel);
+                }
+        }
+
+        private void frmAjedrez_Load(object sender, EventArgs e)
+        {
+            FormarTablero();
+        }
+    }
+}
